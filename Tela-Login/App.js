c754c06e090844fa20969eb861794416116d554d
MatheusAndradeMelo <mandrademelo77@gmail.com>
2021-12-03 14:21:02 -0300

    teste tela de login

---
+++ Tela-Login/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Signin from "./mobile/pages/Signin";  
+
+export default function App() {  
+  return (
+    <>
+      <Signin />
+      <StatusBar style="light" />
+    </>
+  );
+}
+
+//Adicionar biblioteca => yarn ou npm add styled-components
+
