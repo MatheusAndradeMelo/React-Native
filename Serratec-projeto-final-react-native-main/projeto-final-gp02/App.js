34c988f25331e25cf857f7b3d13b0622cd8016e5
MatheusAndradeMelo <mandrademelo77@gmail.com>
2021-12-08 16:52:18 -0300

    atualização do codigo login

--- Serratec-projeto-final-react-native-main/projeto-final-gp02/App.js
+++ Serratec-projeto-final-react-native-main/projeto-final-gp02/App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Signin from "./src/pages/Login";
 
-export default function App() {
+
+export default function App() {  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Signin />
+      <StatusBar style="light" />
+    </> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
+//Adicionar biblioteca => yarn ou npm add styled-components
