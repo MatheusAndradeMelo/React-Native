927fe0fb8b2f41fbe73bca9b5c81c7831f1ced4a
MatheusAndradeMelo <mandrademelo77@gmail.com>
2021-12-09 19:10:23 -0300

    aula api dos gatinhos

--- aula07/App.js
+++ aula07/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button, Image, ActivityIndicator } from "react-native";
 import gato from './assets/gatinho.webp';
 import axios from 'axios';
 
 export default function App() {
 
-const [loading, setLoading] = useState(false);
+const [loading, setLoading] = useState(true);
 const [apiData, setApiData] = useState(null);
 
 const handleClick = async () => {;
@@ -23,11 +23,23 @@ const handleClick = async () => {;
   console.log("TERMINEI A FUNÇÃO"); 
 };
 
+//EXECUTA QUALQUER AÇÃO DEPOIS QUE A PAGINA RENDERIZAR 
+useEffect(() => {
+  handleClick();
+}, []);
+
+//EXECUTA UMA AÇÃO APOS SER RENDERIZADA A PAGINA, E EXECUTA TAMBEM ATRAVES DO ARRAY DE DEPENDENCIA,
+//QUANDO UM STATE É ALTERADO.
+useEffect(() => {
+  console.log("APIDATA ALTERADA");
+}, [apiData]);
+
   return (
     <View style={styles.container}>
       <View style={styles.imgContainer}>
         <Image
-          source={apiData ? { uri: apiData} : gato }
+          //source={apiData ? { uri: apiData} : gato }
+          source={{ uri: apiData }}
           style={styles.img}
           resizeMode="contain"
           onLoadEnd={() => setLoading(false)}
